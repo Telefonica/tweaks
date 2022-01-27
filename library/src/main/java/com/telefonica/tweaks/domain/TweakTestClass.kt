85ed0450839c68ec17190c7d55b254d371a3b5a0
yamal <yamal@tuenti.com>
2022-01-27 14:17:43 +0100

    test

---
+++ library/src/main/java/com/telefonica/tweaks/domain/TweakTestClass.kt
@@ -0,0 +1,12 @@
+package com.telefonica.tweaks.domain
+
+import android.util.Log
+import javax.inject.Inject
+
+class TweakTestClass @Inject constructor() {
+    fun foo() {
+        val a = 1
+        val b = 2
+        Log.d("a + b = ${a + b}")
+    }
+}
