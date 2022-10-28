94373fbbb129d0cf086c3283c82df3da364644a3
gmerino <guillermo.merino.jimenez@telefonica.com>
2022-10-28 10:52:09 +0200

    Fixing background color

--- library/src/enabled/java/com/telefonica/tweaks/ui/theme/Theme.kt
+++ library/src/enabled/java/com/telefonica/tweaks/ui/theme/Theme.kt
@@ -2,10 +2,11 @@ package com.telefonica.tweaks.ui.theme
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun DebugTweaksTheme(
     content: @Composable () -> Unit
 ) {
-    MaterialTheme(colors = TweaksColorPalette, typography = TweaksTypography, content = content)
+    MaterialTheme(colors = TweaksColorPalette.copy(background = Color.Blue), typography = TweaksTypography, content = content)
 }
